074c4c6c7f1d262ae114512f52361f3e8282fe78
Zaher28 <ilbihz@gmail.com>
2024-08-19 16:17:43 -0400

    more collision fixes

--- scripts/States/SebasChase.gd
+++ scripts/States/SebasChase.gd
@@ -10,6 +10,22 @@ func Physics_Update(delta: float):
 	enemy.velocity = direction.normalized() * move_speed
 
 func _on_interactable_block():
+	print("slowing down!")
 	move_speed /= 2
 	await get_tree().create_timer(2).timeout
 	move_speed *= 2
+	print("speeding up!")
+
+func _on_cracked_wall_block():
+	print("slowing down!")
+	move_speed /= 2
+	await get_tree().create_timer(2).timeout
+	move_speed *= 2
+	print("speeding up!")
+
+func _on_interactable_2_block():
+	print("slowing down!")
+	move_speed /= 2
+	await get_tree().create_timer(2).timeout
+	move_speed *= 2
+	print("speeding up!")
